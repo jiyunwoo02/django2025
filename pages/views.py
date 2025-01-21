14172b3e3fdc612747037559b0aceb5e96e62362
Jiyun Woo <jiyunwoo33@gmail.com>
2025-01-21 14:58:23 +0900

    3주차 django 개발 완료 (3)

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Inquiry
+from django.contrib import messages
 
 def mainpage(request):
     return render(request, 'pages/mainpage.html')
@@ -16,9 +18,13 @@ def submit_inquiry(request):
         email = request.POST.get('email')
         message = request.POST.get('message')
 
-        # 데이터베이스에 저장
-        # Inquiry.objects.create(name=name, email=email, message=message)
+        # 데이터베이스에 저장 -> admin 페이지에서 확인
+        Inquiry.objects.create(name=name, email=email, message=message)
 
+        # 성공 메시지
+        messages.success(request, "⭕ 문의가 성공적으로 접수되었습니다.")
         return redirect('/support/')
     else:
+        # 오류 메시지
+        messages.error(request, "❌ 잘못된 요청입니다. 다시 시도해주세요.")
         return redirect('/support/')
